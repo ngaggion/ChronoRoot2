72a6a2b9dbb8f0892eca816a6718354b8e48afc7
Nicolás Gaggion <ngaggion@gmail.com>
2026-01-09 15:13:11 +0100

    bind running user for windows

--- apptainerInstaller/installer_windows.sh
+++ apptainerInstaller/installer_windows.sh
@@ -119,7 +119,7 @@ main() {
 #!/bin/bash
 [ -z "\$DISPLAY" ] && export DISPLAY=:0
 echo "Starting $name..."
-apptainer exec $GPU_FLAG --bind /mnt:/mnt --env DISPLAY=\$DISPLAY "$IMAGE_PATH" \\
+apptainer exec $GPU_FLAG --bind /mnt:/mnt --env DISPLAY=\$DISPLAY --bind /run/user/$(id -u):/run/user/$(id -u) "$IMAGE_PATH" \\
   bash -c "source /opt/conda/etc/profile.d/conda.sh && conda activate ChronoRoot && cd $REPO_DIR/$folder && python run.py"
 EOF
         chmod +x "$sh_file"
